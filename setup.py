128c18732dfa0933633eaf04f9ee27fe306f02bd
Chapaneri, Santosh <Santosh.Chapaneri@wolterskluwer.com>
2023-06-09 09:51:29 +0530

    updated name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(
-    name='cookiecutter-pypackage',
+    name='cookiecutter-pypackage-sc',
     packages=[],
     version='0.1.0',
     description='Cookiecutter template for a Python package',
